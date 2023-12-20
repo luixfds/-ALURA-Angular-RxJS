758357f3dab654ee283be7fa013383c76d5ea25b
luixfds <l.fds@yahoo.com>
2023-12-20 12:47:30 -0300

    aula 5.7 debouncetime

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Subscription, filter, map, switchMap, tap } from 'rxjs';
+import { Subscription, debounceTime, filter, map, switchMap, tap } from 'rxjs';
 import { Item, Livro } from 'src/app/models/books-DTO';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivrosService } from 'src/app/services/livros.service';
+
+const searchTime = 300
 
 @Component({
   selector: 'app-lista-livros',
@@ -57,6 +59,7 @@ export class ListaLivrosComponent implements OnInit {
 // observable que faz a requisicao dinamica somente com o ultimo valor digitado
   livrosEncontrados$ = this.campoBusca.valueChanges
   .pipe(
+    debounceTime(searchTime),
     filter((valorDigitado) => valorDigitado.length >= 3),
     tap(() => console.log("fluxo inicial")),
     switchMap((valorDigitado) => this.livrosService.getLivros(valorDigitado)),
